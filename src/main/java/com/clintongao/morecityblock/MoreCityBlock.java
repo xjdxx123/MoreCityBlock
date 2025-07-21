fc22028389cd11b8dedc2433711dbae2d94a9206
Clinton Gao <yifeng.gao@mail.mcgill.ca>
2025-07-20 20:10:34 -0400

    initial commit

---
+++ src/main/java/com/clintongao/morecityblock/MoreCityBlock.java
@@ -0,0 +1,12 @@
+package com.clintongao.morecityblock;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(MoreCityBlock.MODID)
+public class MoreCityBlock {
+    public static final String MODID = "morecityblock";
+
+    public MoreCityBlock() {
+        System.out.println("Hello from MoreCityBlock mod!");
+    }
+}
