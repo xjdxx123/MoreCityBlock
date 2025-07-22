f375cc5bf5511ad09789029e5e3a3d1f3960b29a
Clinton Gao <yifeng.gao@mail.mcgill.ca>
2025-07-21 21:34:41 -0400

    路灯

--- src/main/java/com/clintongao/morecityblock/MoreCityBlock.java
+++ src/main/java/com/clintongao/morecityblock/MoreCityBlock.java
@@ -1,16 +1,16 @@
 package com.clintongao.morecityblock;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
 @Mod(MoreCityBlock.MODID)
 public class MoreCityBlock {
+
     public static final String MODID = "morecityblock";
 
-    public MoreCityBlock() {
+    public MoreCityBlock(IEventBus modEventBus) {
+        ModBlocks.register(modEventBus);
         System.out.println("Hello from MoreCityBlock mod!");
     }
 }
-
-/*
-前者是傻波一
- */
